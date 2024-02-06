c7481c52f53cd558ca32f881c519b7bce865f3db
robcicle <robbie.jack.atkin@gmail.com>
2024-02-06 16:12:45 +0000

    ItemSlot.cs comments added

--- Assets/Scripts/Managers/Game/Inventory/ItemSlot.cs
+++ Assets/Scripts/Managers/Game/Inventory/ItemSlot.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -8,35 +6,38 @@ using UnityEngine.EventSystems;
 public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     // Item Data
-    public string itemName;
-    public string itemDescription;
-    public int quantity;
-    public Sprite itemIcon;
-    public bool isFull;
+    public string itemName;          // Name of the item
+    public int quantity;             // Quantity of the item
+    public bool isFull;              // Flag indicating if the slot is full
+    private string itemDescription;  // Description of the item
+    private Sprite itemIcon;         // Icon representing the item
 
     // Slot Data
     [SerializeField]
-    private TMP_Text quantityText;
+    private TMP_Text quantityText;  // Text displaying the quantity of the item in the slot
     [SerializeField]
-    private Image itemImage;
+    private Image itemImage;        // Image representing the item
 
-    public GameObject selectedPanel;
-    public bool isItemSelected = false;
+    [SerializeField]
+    private GameObject selectedPanel;     // Panel indicating the selected item
+    private bool isItemSelected = false;  // Flag indicating if the item is selected
 
-    private InventoryManager inventoryManager;
+    private InventoryManager inventoryManager;  // Reference to the InventoryManager
 
     private void Start()
     {
+        // Find and get a reference to the InventoryManager
         inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
     }
 
+    // Add an item to the slot with a specified quantity
     public int AddItem(ItemSO item, int quantity)
     {
-        // Check to see if slot is full
+        // Check if the slot is full
         if (isFull)
             return quantity;
 
-        // Update Slot Data
+        // Update slot data with item information
         this.itemName = item.itemName;
         this.itemDescription = item.itemDescription;
         this.itemIcon = item.itemIcon;
@@ -48,7 +49,7 @@ public class ItemSlot : MonoBehaviour, IPointerClickHandler
         this.quantity += quantity;
         if (this.quantity >= inventoryManager.maxItemsNum)
         {
-            // Return leftovers
+            // Handle excess items beyond maximum allowed
             int extraItems = this.quantity - inventoryManager.maxItemsNum;
             this.quantity = inventoryManager.maxItemsNum;
 
@@ -65,26 +66,37 @@ public class ItemSlot : MonoBehaviour, IPointerClickHandler
         return 0;
     }
 
+    // Deselect the item slot
+    public void DeselectSlot()
+    {
+        selectedPanel.SetActive(false);  // Deactivate the selected panel
+        isItemSelected = false;          // Set isItemSelected flag to false
+    }
+
+    // Handle left click action
     void OnLeftClick()
     {
         if (isItemSelected)
         {
-            inventoryManager.UseItem(itemName);
+            inventoryManager.UseItem(itemName); // Use the item if it's already selected
             return;
         }
 
+        // Deselect all other slots and select this one
         inventoryManager.DeselectAllSlots();
         selectedPanel.SetActive(true);
         isItemSelected = true;
 
+        // Update item description data
         inventoryManager.UpdateDescriptionData(itemName, itemDescription, itemIcon);
     }
 
+    // Handle pointer click event
     public void OnPointerClick(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Left)
         {
-            OnLeftClick();
+            OnLeftClick(); // Perform left click action
         }
     }
 }
