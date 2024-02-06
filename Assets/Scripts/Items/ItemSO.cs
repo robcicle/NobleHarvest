a55e7552bd71fcb7f230eb0ff6a7e4e527b1c1ca
robcicle <robbie.jack.atkin@gmail.com>
2024-02-06 16:09:47 +0000

    ItemSO.cs comments added

--- Assets/Scripts/Items/ItemSO.cs
+++ Assets/Scripts/Items/ItemSO.cs
@@ -1,27 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class ItemSO : ScriptableObject
 {
-    public string itemName;
-    public string itemDescription;
-    public int defaultQuantity = 1;
-    public Sprite itemIcon;
+    // Item Data
+    public string itemName;          // Name of the item
+    public string itemDescription;   // Description of the item
+    public int defaultQuantity = 1;  // Default quantity of the item
+    public Sprite itemIcon;          // Icon representing the item
 
-    public InventoryManager.ItemCategories itemCategory = new InventoryManager.ItemCategories();
+    // Item Category
+    public InventoryManager.ItemCategories itemCategory = new InventoryManager.ItemCategories();  // Category of the item
 
+    // Use the item based on its category
     public void UseItem()
     {
-        switch (itemCategory) 
+        // Switch case to determine the action based on item category
+        switch (itemCategory)
         {
-            case InventoryManager.ItemCategories.Seeds:
+            case InventoryManager.ItemCategories.Seeds:  // If the item category is Seeds
                 break;
-            case InventoryManager.ItemCategories.Weapons:
-                Debug.Log(itemName + " was used.");
+            case InventoryManager.ItemCategories.Weapons:  // If the item category is Weapons
+                Debug.Log(itemName + " was used.");  // Log that the item was used
                 break;
-            default:
+            default:  // If the item category is not recognized
+                Debug.LogError(itemName + " uses unrecognized category '" + itemCategory + "'");  // Log unrecognized category as an error
                 break;
         }
     }
